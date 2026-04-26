27e9deb351eba4c24f352dbfc271046fea67ab07
afunky11 <allanfunknissen@gmail.com>
2026-04-26 20:24:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
 
+// Supabase konfiguration
+// Disse værdier er offentlige (publishable key er designet til at ligge i klient)
+// RLS i databasen sikrer at adgangen er begrænset
+
+const SUPABASE_URL = 'https://rljwhngtzhefrtvmfpyh.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_Up9cevlFCt4gY2f7-TDdsg_eG1cgWHC';
+const SUPABASE_BUCKET = 'fakturaer';
